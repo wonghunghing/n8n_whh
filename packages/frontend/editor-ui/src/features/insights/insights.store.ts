d30fd3248e80624891dfd19a42148f12c6d81d70
Csaba Tuncsik <csaba.tuncsik@gmail.com>
2025-03-14 14:44:20 +0100

    feat(editor): Rebasing insights banner
    # Conflicts:
    #	packages/frontend/editor-ui/src/features/insights/insights.store.ts

--- packages/frontend/editor-ui/src/features/insights/insights.store.ts
+++ packages/frontend/editor-ui/src/features/insights/insights.store.ts
@@ -4,6 +4,30 @@ import * as insightsApi from '@/features/insights/insights.api';
 import { useRootStore } from '@/stores/root.store';
 import { transformInsightsSummary } from '@/features/insights/insights.utils';
 
+import {
+	getDatesArrayFromToday,
+	getRandomIntBetween,
+	randomCumulativeData,
+	randomDataPoint,
+} from '@/features/insights/chartjs.utils';
+
+
+export type Count = { date: string; count: number };
+
+export type CountResponse = {
+	total: {
+		failure: Count[];
+		success: Count[];
+	};
+	failed: Count[];
+	failureRate: Count[];
+	timeSaved: {
+		average: Count[];
+		median: Count[];
+	};
+	runTime: Count[];
+};
+
 export const useInsightsStore = defineStore('insights', () => {
 	const rootStore = useRootStore();
 
@@ -12,7 +36,37 @@ export const useInsightsStore = defineStore('insights', () => {
 		return transformInsightsSummary(raw);
 	}, []);
 
+	const fetchCounts = async ({ time_span }: { time_span: number }): Promise<CountResponse> => {
+		const dates = getDatesArrayFromToday(Number(time_span));
+		return {
+			total: {
+				failure: dates.map((date) => ({
+					date,
+					count: randomDataPoint(400),
+				})),
+				success: dates.map((date) => ({
+					date,
+					count: randomDataPoint(400),
+				})),
+			},
+			failed: dates.map((date) => ({
+				date,
+				count: randomDataPoint(400),
+			})),
+			failureRate: dates.map((date) => ({
+				date,
+				count: randomDataPoint(400),
+			})),
+			timeSaved: {
+				average: randomCumulativeData(dates, 3),
+				median: randomCumulativeData(dates, 4),
+			},
+			runTime: dates.map((date) => ({ date, count: getRandomIntBetween(0.5, 4) })),
+		};
+	};
+
 	return {
 		summary,
+		fetchCounts,
 	};
 });
